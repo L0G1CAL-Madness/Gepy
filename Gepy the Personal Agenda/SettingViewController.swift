ead152bd8927680119606dc7d1538f8e1a3966ec
Jayden Bulexa <jaydenbulexa@Jaydens-iMac.local>
2017-09-10 11:25:58 -0700

    Capture class details working

--- Gepy the Personal Agenda/SettingViewController.swift
+++ Gepy the Personal Agenda/SettingViewController.swift
@@ -9,23 +9,19 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-    
+    // Connect Text Fields
     @IBOutlet weak var firstClass: UITextField!
-
     @IBOutlet weak var secondClass: UITextField!
-    
     @IBOutlet weak var thirdClass: UITextField!
-    
     @IBOutlet weak var fourthClass: UITextField!
-    
     @IBOutlet weak var fifthClass: UITextField!
-    
     @IBOutlet weak var sixthClass: UITextField!
-    
     @IBOutlet weak var seventhClass: UITextField!
     
+    // Save on Button Press
     @IBAction func onSaveButton(_ sender: Any) {
-            var classArray = [AnyObject]()
+        //Create Array and append text to array
+        var classArray = [AnyObject]()
             classArray.append(firstClass.text as AnyObject)
             classArray.append(secondClass.text as AnyObject)
             classArray.append(thirdClass.text as AnyObject)
@@ -33,27 +29,36 @@ class SettingViewController: UIViewController {
             classArray.append(fifthClass.text as AnyObject)
             classArray.append(sixthClass.text as AnyObject)
             classArray.append(seventhClass.text as AnyObject)
-            print(classArray)
+        
+        //Save to memory
         UserDefaults.standard.set(classArray, forKey: "savedClassDetails")
+
+        //Present the home page storyboard up
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "mainVC") as! ViewController
+        
         let navController = UINavigationController(rootViewController: vc)
          self.present(navController, animated: true, completion: nil)
     }
     
-    
-
-    
    override func viewDidLoad() {
         super.viewDidLoad()
     
+    //Change the NavBar attributes
     self.navigationController?.navigationBar.alpha = 0
-   
     self.navigationController?.navigationBar.barTintColor = UIColor (red: 38/255, green: 38/255, blue: 38/255, alpha: 1.0)
-   
     self.navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "impact", size: 30)!, NSForegroundColorAttributeName : UIColor.white]
-    
-       let array_get = UserDefaults.standard.array(forKey: "savedClassDetails") ?? [String]()
-    print(array_get[0])
+
+    //Call back the selections from last time to fill text fields
+    let array_get = UserDefaults.standard.array(forKey: "savedClassDetails") ?? [String]()
+
+    //Fill text fields from previous enter
+   firstClass.text = array_get[0] as? String
+   secondClass.text = array_get[1] as? String
+   thirdClass.text = array_get[2] as? String
+   fourthClass.text = array_get[3] as? String
+   fifthClass.text = array_get[4] as? String
+   sixthClass.text = array_get[5] as? String
+   seventhClass.text = array_get[6] as? String
     }
 
  
